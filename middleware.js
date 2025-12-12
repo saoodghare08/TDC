9423ef3d1ebc638a0e8c3a301f4f28c79fd19755
saood ghare <saoodghare0809@gmail.com>
2025-12-12 14:38:31 +0400

    refactor: Update middleware matcher to exclude static assets and image files.

--- middleware.js
+++ middleware.js
@@ -6,7 +6,13 @@ export function middleware(request) {
 
 export const config = {
     matcher: [
-        '/admin/:path*',
-        '/login'
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * - public folder
+         */
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
