bb36fdad928a65a3b18f86046f0ddf74c32eea1b
saood ghare <saoodghare0809@gmail.com>
2025-12-16 13:01:05 +0400

    feat: add root layout with dynamic SEO metadata, font configuration, and analytics integration.

--- app/layout.js
+++ app/layout.js
@@ -47,6 +47,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${workSans.variable} ${poppins.variable} font-sans antialiased bg-gray-50`}
       >
+        {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-1D06V6TYFC"
           strategy="afterInteractive"
@@ -60,6 +61,30 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-1D06V6TYFC');
           `}
         </Script>
+        {/* End Google Analytics */}
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5MDWV8L5');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5MDWV8L5"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Providers>
           {children}
         </Providers>
